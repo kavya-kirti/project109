e5476a82cd0791faea9626d2359b63615e55eb7a
kavya-kirti <138196399+kavya-kirti@users.noreply.github.com>
2024-06-21 18:44:56 +0530

    Add files via upload

---
+++ take_screenshot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pyautogui
+import cv2
+import imutils
+
+
+image = pyautogui.screenshot()
+
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+cv2.imwrite("in_memory_to_disk.png", image)
+
+pyautogui.screenshot("straight_to_disk.png")
+
+image = cv2.imread("straight_to_disk.png")
+
+cv2.imshow("Screenshot", imutils.resize(image, width=600))
